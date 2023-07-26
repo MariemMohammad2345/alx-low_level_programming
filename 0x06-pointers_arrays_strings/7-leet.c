87ccdfe2691a2aa9969a1cd8987cd7208653eba2
MariemMohammad2345 <mmh7775886@gmail.com>
2023-07-26 08:47:24 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,16 +6,16 @@
  * Return: String that is encoded
  */
 
-	char *leet(char *c)
-	{
-		char *cp = c;
+char *leet(char *c)
+{
+	char *cp = c;
 	char key[] = {'A', 'E', 'O', 'T', 'L'};
 	int value[] = {4, 3, 0, 7, 1};
 	unsigned int i;
 
 	while (*c)
 	{
-		for (i = 0; < sizeof(key) / sizeof(char); i++)
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
 			/*32 is the difference between lower case letters and apper case letters*/
 			if (*c == key[i] || *c == key[i] + 32)
@@ -28,4 +28,4 @@
 
 	return (cp);
 
-	}
+}
